528f62b6e4a7cffc21165bd29d41b1b1139d7539
sungwkim <sungwkim@curos.co.kr>
2012-12-17 18:52:18 +0900

    Create class MyFrame.

---
+++ CurosWxWidgets/MyFrame.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+// The Main Header
+#include "MyFrame.h"
+
+
+
+MyFrame::MyFrame()
+{
+}
+
+
+
+MyFrame::~MyFrame()
+{
+}
---
+++ CurosWxWidgets/MyFrame.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+class MyFrame
+{
+public:
+	MyFrame() ;
+	~MyFrame() ;
+};
