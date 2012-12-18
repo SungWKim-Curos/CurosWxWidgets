5de6359fe704059190fbef9333a08f66eff31272
DuncanSungWKim <DuncanSungWKim@gmail.com>
2012-12-18 13:40:39 +0900

    Create the status bar.

--- CurosWxWidgets/MyFrame.cpp
+++ CurosWxWidgets/MyFrame.cpp
@@ -8,6 +8,8 @@
 MyFrame::MyFrame( const wxString& a_title, const wxPoint& a_pos, const wxSize& a_size )
 	: wxFrame( NULL, -1, a_title, a_pos, a_size )
 {
+	CreateStatusBar() ;
+	SetStatusText( _("Welcome to wxWidgets!") ) ;
 }
 
 
